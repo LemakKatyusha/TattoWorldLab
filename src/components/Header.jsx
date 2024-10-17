692bad4e412af9844d6d42ce861454e81aff5d5e
LemakKatyusha <lemak.kateryna@student.uzhnu.edu.ua>
2024-10-17 21:36:37 +0300

    test2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react';
 
-const Header = () => {
-  return (
-    <header>
-      <h2>Site Header</h2>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
+const Heer = () => {
+	return (
+		<header>
+			<h2>Site Header</h2>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
