b14c983e7686c0d180f3c73aebd2a5a7b18d92bc
LemakKatyusha <lemak.kateryna@student.uzhnu.edu.ua>
2024-10-24 10:19:57 +0300

    hucky

--- src/components/ChangeProfileForm.jsx
+++ src/components/ChangeProfileForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/changeprofileform.css';
 
-const ngeProfileForm = () => {
+const ChangeProfileForm = () => {
 	return (
 		<div className="form">
 			<div className="imgchange">
